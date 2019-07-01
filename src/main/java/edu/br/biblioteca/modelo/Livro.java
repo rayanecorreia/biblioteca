62a25b5f7f5c3f6abfce09e98ae73b5901804de2
naomy leticia <naomyleticia96@gmail.com>
2019-07-01 15:52:47 -0300

    adicao do atributo editora na classe livro

--- src/main/java/edu/br/biblioteca/modelo/Livro.java
+++ src/main/java/edu/br/biblioteca/modelo/Livro.java
@@ -5,9 +5,23 @@ public class Livro {
 	private String titulo;
 	private int numPaginas;
 	private String autor;
+	private String editora;
 	
 	
 	
+	
+	/**
+	 * @return the editora
+	 */
+	public String getEditora() {
+		return editora;
+	}
+	/**
+	 * @param editora the editora to set
+	 */
+	public void setEditora(String editora) {
+		this.editora = editora;
+	}
 	public Livro(String titulo, String autor) {
 		super();
 		this.titulo = titulo;
