13d4ba75ab74aaf1c51cbb1d84cee11a29da330d
naomy leticia <naomyleticia96@gmail.com>
2019-07-01 16:28:03 -0300

    testando App

--- src/main/java/edu/br/biblioteca/App.java
+++ src/main/java/edu/br/biblioteca/App.java
@@ -1,5 +1,7 @@
 package edu.br.biblioteca;
 
+import edu.br.biblioteca.modelo.Usuario;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        Usuario usuario =  new Usuario();
+        usuario.setEmail("ana@oi.com");
+        usuario.setMatricula("1235");
+        usuario.setSenha("ana123");
     }
 }
